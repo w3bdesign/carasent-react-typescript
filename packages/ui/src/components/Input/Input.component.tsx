8e9883efbd1c930f1d0eee0332a5bbd1b80cb6cb
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-07 20:39:30 +0100

    ♿️ Aria invalid for Input

--- packages/ui/src/components/Input/Input.component.tsx
+++ packages/ui/src/components/Input/Input.component.tsx
@@ -26,15 +26,18 @@ export const Input = ({
     formState: { errors },
   } = useFormContext();
 
+  const inputHasErrors = errors[name] ? true : false;
+
   return (
     <>
       <label htmlFor={name} className="block mt-4">
         <span className="block mb-1 text-gray-600">{labelText}</span>
         <input
-          {...register(name)}
           placeholder={placeholder}
           className="w-64 px-4 py-2 mt-2 text-base bg-white border border-gray-400 rounded focus:outline-none focus:border-black"
           id={name}
+          aria-invalid={inputHasErrors}
+          {...register(name)}
         />
         {errors[name as string] && (
           <span className="text-red-500 text-xl p-2 block mt-2">
