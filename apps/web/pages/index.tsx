7b970b0b9dfe52b68299d289c65dc6eec2becadf
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-09 04:16:25 +0100

    ✨ Priority som prop i Character

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         <div className="bg-gray-100 min-h-screen">
           <div className="max-w-2xl w-full mx-auto px-4 py-16">
             <Header title="Carasent - React / Typescript oppgave" />
-            <Character id={2} />
+            <Character priority id={2} />
             <Form<TFormSchemaType> onSubmit={onSubmit} methods={methods}>
               <Input name="firstName" labelText="Navn" />
               <Select
