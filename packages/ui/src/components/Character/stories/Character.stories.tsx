2eaae678a61dc7d273ab6579cfa6ef9eba01a128
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-20 03:51:13 +0100

    ✨ Character Storybook

---
+++ packages/ui/src/components/Character/stories/Character.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Character, ICharacterProps } from '../Character.component';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/UI/Character',
+  component: Character,
+  argTypes: {
+    id: {
+      description: 'ID på Character',
+      control: 'number',
+    },
+    priority: {
+      description: 'Prioritet loading på Character',
+      control: 'boolean',
+    },
+  },
+} as ComponentMeta<typeof Character>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Character> = ({
+  id,
+  priority,
+}: ICharacterProps) => <Character id={id} priority={priority} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  id: 2,
+  priority: true,
+};
