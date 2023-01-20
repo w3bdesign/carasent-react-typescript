6c847a731ff5bf35c1c146ef3d0559033e72ed5e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-01-20 02:16:25 +0100

    ✨ Radio Storybook

--- packages/ui/src/components/Radio/stories/Radio.stories.tsx
+++ packages/ui/src/components/Radio/stories/Radio.stories.tsx
@@ -1,30 +1,15 @@
-import { Fragment } from 'react';
-import { useFormContext } from 'react-hook-form';
+import React, { Fragment } from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-export interface IRadioProps {
+interface IRadioProps {
   options: string[];
   labelText: string;
   name: string;
 }
 
-export type TOptionsValue = string;
+type TOptionsValue = string;
 
-/**
- * Radio component used for selecting data in form
- * @function Radio
- * @param {string[]} options Array of options 
- * @param {string} labelText - Label text for select
- * @param {string} name - Name of radio element
-
- * @returns {JSX.Element} - Rendered component
- */
-
-export const Radio = ({ options, labelText, name, ...rest }: IRadioProps) => {
-  const {
-    register,
-    formState: { errors },
-  } = useFormContext();
-
+const Radio = ({ name, options, labelText }: IRadioProps): JSX.Element => {
   return (
     <>
       <div className="mx-auto text-center w-64">
@@ -40,8 +25,6 @@ export const Radio = ({ options, labelText, name, ...rest }: IRadioProps) => {
                   value={value}
                   id={value}
                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  {...register(name)}
-                  {...rest}
                 />
                 <label
                   htmlFor={value}
@@ -53,12 +36,44 @@ export const Radio = ({ options, labelText, name, ...rest }: IRadioProps) => {
             </Fragment>
           ))}
         </fieldset>
-        {errors[name] && (
-          <span className="text-red-600 text-xl p-2 block mt-2">
-            {String(errors[name]?.message)}
-          </span>
-        )}
+        <span className="text-red-600 text-xl p-2 block mt-2">
+          Eksempel feilmelding
+        </span>
       </div>
     </>
   );
+};
+
+export default {
+  title: 'Components/UI/Radio',
+  component: Radio,
+  argTypes: {
+    name: {
+      description: 'Navn på komponent',
+    },
+    options: {
+      description: 'Primary or secondary button variant',
+      options: ['primary', 'secondary'],
+      control: 'array',
+    },
+    labelText: {
+      description: 'Tekst på label',
+    },
+  },
+} as ComponentMeta<typeof Radio>;
+
+const Template: ComponentStory<typeof Radio> = ({
+  name,
+  options,
+  labelText,
+}: IRadioProps) => (
+  <Radio name={name} options={options} labelText={labelText} />
+);
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  name: 'Tekst',
+  options: ['Option #1', 'Option #2'],
+  labelText: 'Label tekst',
 };
